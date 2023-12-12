c5d5be1eacd44ae3dc26f19dddb0bb748c5e2e6e
Yonela Johannes <johannesyonela@gmail.com>
2023-12-12 20:23:02 +0200

    chore: add api url

--- client/src/constants/base_urls.js
+++ client/src/constants/base_urls.js
@@ -1,7 +1,7 @@
 let apiUrl;
 
 if (process.env.NODE_ENV === 'production') {
-  apiUrl = 'https://myblog-707i.onrender.com/api/';
+  apiUrl = 'https://pinky-api.onrender.com/api/';
 } else {
   apiUrl = 'http://localhost:4000/api/';
 }
