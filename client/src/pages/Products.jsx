bce39fa7ed84d09bff4a7a38e1e64b4cb63b26f6
Yonela Johannes <johannesyonela@gmail.com>
2024-03-20 18:55:52 +0200

    Chore: load data on state

--- client/src/pages/Products.jsx
+++ client/src/pages/Products.jsx
@@ -1,11 +1,21 @@
 import MaxWidthWrapper from '../components/MaxWidthWrapper'
 import ProductReel from '../components/ProductReel'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from "react-redux";
 import Items from '../components/Products/Products'
 import Loader from '../components/shared/Loader'
+import { getProducts } from '../redux/features/products/productSlice'
+import { useEffect, useState } from "react";
 
 const Products = () => {
   const { items , loading} = useSelector((state) => state.products);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getProducts());
+  }, []);
+
+  console.log(loading)
+  
   return (
     <MaxWidthWrapper>
       {loading ? (
