08f5ee349df7f31858166284c9c58feedfa55c0c
yonela-johannes <johannesyonela@gmail.com>
2024-10-17 14:10:13 +0200

    chore: set timeout

--- backend/index.js
+++ backend/index.js
@@ -62,7 +62,11 @@ app.use(handleError)
 
 app.listen(PORT, () =>
 {
-  mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 5000
+  })
     .then(() => console.log("db connected"))
     .catch((err) => console.log(err.message));
   console.log(`Server is running at http://localhost:${PORT}`)
